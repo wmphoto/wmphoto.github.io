ef5989cb34b792f5bf22e0990d1f80c1bf7c1a2b
LynetteShen <32914298+LynetteShen@users.noreply.github.com>
2017-10-25 19:54:33 -0400

    please dont kill it

--- data.js
+++ data.js
@@ -7,4 +7,12 @@ data = [
     "imgLarge": "rx01-large.jpg",
     "date": "2017-10-25"
   },
+  {
+    "id": "ls",
+    "x": 1,
+    "y": 0,
+    "imgSmall": "ls01-small.jpg",
+    "imgLarge": "ls01-large.jpg",
+    "date": "2017-10-25"
+  },
 ];
