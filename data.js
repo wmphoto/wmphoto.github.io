6f0a4883f8bb92fd08a7ca690345a7d258af4d0f
lillianzhaoart <32914281+lillianzhaoart@users.noreply.github.com>
2017-10-30 20:02:08 -0400

    delete image

--- data.js
+++ data.js
@@ -90,21 +90,12 @@ data = [
     "seq": 10,
   },
   {
-    "id": "lz",
-    "x": -1,
-    "y": -1,
-    "imgSmall": "lz03-small.jpg",
-    "imgLarge": "lz03-large.jpg",
-    "date": "2017-10-30",
-    "seq": 11,
-  },
-  {
     "id": "ja",
     "x": 0,
     "y": 1,
     "imgSmall": "ja01-small.jpg",
     "imgLarge": "ja01-large.jpg",
     "date": "2017-10-30",
-    "seq": 12,
+    "seq": 11,
   },
 ];
